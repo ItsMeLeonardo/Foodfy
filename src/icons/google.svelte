3475c0cb2b9be0d09555918bb6cf023a87c2ed73
Carlos Leonardo <74982565+ItsMeLeonardo@users.noreply.github.com>
2022-09-10 15:28:21 -0500

    icons: facebook, google, twitter

---
+++ src/icons/google.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	export let w = 24
+	export let h = 24
+</script>
+
+<svg
+	xmlns="http://www.w3.org/2000/svg"
+	class="icon icon-tabler icon-tabler-brand-google"
+	width={w}
+	height={h}
+	viewBox="0 0 24 24"
+	stroke-width="1.5"
+	stroke="currentColor"
+	fill="none"
+	stroke-linecap="round"
+	stroke-linejoin="round"
+>
+	<path stroke="none" d="M0 0h24v24H0z" fill="none" />
+	<path d="M17.788 5.108a9 9 0 1 0 3.212 6.892h-8" />
+</svg>
