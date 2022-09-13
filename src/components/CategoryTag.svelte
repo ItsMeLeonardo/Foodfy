09bf1b827cae94a150a5904846aa868ffb1b0b61
Carlos Leonardo <74982565+ItsMeLeonardo@users.noreply.github.com>
2022-09-12 19:30:28 -0500

    FEAT: add other tagCategory type

--- src/components/CategoryTag.svelte
+++ src/components/CategoryTag.svelte
@@ -3,20 +3,35 @@
 
 	// export let category: string
 	export let label: string
+	export let canSearch: boolean = false
+
+	export let clickeable: boolean = false
 </script>
 
-<a href={`/`} class="tag">
-	<span class="text">{label}</span>
-	<span class="options">
-		<Search h={12} w={12} />
-	</span>
-</a>
+{#if canSearch}
+	<a href={`/`} class="tag">
+		<span class="text">{label}</span>
+		<span class="options">
+			<Search h={12} w={12} />
+		</span>
+	</a>
+{:else}
+	<div class="tag" class:clickeable on:click>
+		<span class="text">{label}</span>
+		<span class="options">
+			<slot name="icon"><!-- optional fallback --></slot>
+		</span>
+	</div>
+{/if}
 
 <style lang="postcss">
+	.clickeable {
+		cursor: pointer;
+	}
 	.tag {
 		color: var(--white);
 		font-size: 10px;
-		padding: 0.25rem 0.5rem 0.25rem 0.25rem;
+		padding: 0.25rem 0.75rem 0.25rem 0.25rem;
 		border-radius: 0.25rem 1rem 1rem 0.25rem;
 		background-color: var(--primary);
 		max-width: 100px;
