d5cfffe2001fa7b773b2164d1ad9e6ecf07e869a
Carlos Leonardo <74982565+ItsMeLeonardo@users.noreply.github.com>
2022-09-14 19:55:46 -0500

    FEAT: add checkbox component

---
+++ src/components/general/Checkbox.svelte
@@ -0,0 +1,102 @@
+<!-- checkbox component with svelte -->
+<script lang="ts">
+	import { onMount } from 'svelte'
+
+	import { fade } from 'svelte/transition'
+	import Check from '$icons/Check.svelte'
+
+	export let name: string = ''
+	export let checked = false
+	export let value: string | object = ''
+	export let group: any[] = []
+
+	const existThisValue = group.some((item) => {
+		return JSON.stringify(item) == JSON.stringify(value)
+	})
+
+	function toogleValue(checked: boolean) {
+		if (checked) {
+			group = [value, ...group]
+			return
+		}
+
+		group = group.filter((item) => {
+			return JSON.stringify(item) !== JSON.stringify(value)
+		})
+	}
+
+	function onInput(event: Event) {
+		if (!group) return
+		const input = event.target as HTMLInputElement
+		const checked = input.checked
+		toogleValue(checked)
+	}
+
+	onMount(() => {
+		if (checked) {
+			group.push(value)
+			group = group
+		}
+		if (existThisValue) {
+			checked = true
+		}
+	})
+</script>
+
+<label class="checkbox-container" tabindex="0">
+	<input type="checkbox" {name} hidden bind:checked on:input={onInput} />
+	<div class="icon-container" class:checked>
+		{#if checked}
+			<span class="icon" transition:fade={{ duration: 0.2 }}>
+				<slot name="icon">
+					<Check w={16} h={16} weight="bold" />
+				</slot>
+			</span>
+		{/if}
+	</div>
+	<span class="label">
+		<slot><!-- optional fallback --></slot>
+	</span>
+</label>
+
+<style lang="postcss">
+	.checkbox-container {
+		width: max-content;
+		display: flex;
+		align-items: center;
+		gap: 0.5rem;
+		cursor: pointer;
+	}
+	.icon-container {
+		--size: 24px;
+		width: var(--size);
+		height: var(--size);
+		border-radius: 8px;
+		border: 2px solid var(--gray-light);
+		transition: background 0.3s ease-in-out;
+
+		.icon {
+			width: 100%;
+			height: 100%;
+			display: flex;
+			justify-content: center;
+			align-items: center;
+			color: var(--white);
+		}
+
+		&.checked {
+			background: var(--primary);
+			border: 2px solid var(--primary);
+			box-shadow: 0 0.5rem 1rem -0.25rem var(--primary);
+		}
+	}
+
+	.icon * {
+		opacity: 0;
+		transition: opacity 0.2s ease-in-out;
+	}
+
+	.label {
+		font-size: 14px;
+	}
+</style>
---
+++ src/icons/Check.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	export let w = 20
+	export let h = 20
+	export let weight: 'light' | 'bold' = 'light'
+
+	const strokeWidth = weight === 'light' ? 1.5 : 4
+</script>
+
+<svg
+	xmlns="http://www.w3.org/2000/svg"
+	class="icon icon-tabler icon-tabler-check"
+	width={w}
+	height={h}
+	viewBox="0 0 24 24"
+	stroke-width={strokeWidth}
+	stroke="currentColor"
+	fill="none"
+	stroke-linecap="round"
+	stroke-linejoin="round"
+>
+	<path stroke="none" d="M0 0h24v24H0z" fill="none" />
+	<path d="M5 12l5 5l10 -10" />
+</svg>
