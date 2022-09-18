dbce75de41a6edf9c9b9ffbeed5b8af4781eb39f
Carlos Leonardo <74982565+ItsMeLeonardo@users.noreply.github.com>
2022-09-18 17:50:42 -0500

    add icons

---
+++ src/icons/Chart.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+	export let size: number | null = null
+	export let w = 20
+	export let h = 20
+	export let weight: 'light' | 'bold' = 'light'
+
+	const strokeWidth = weight === 'light' ? 1.5 : 4
+</script>
+
+<svg
+	width={size || w}
+	height={size || h}
+	stroke-width={strokeWidth}
+	viewBox="0 0 16 16"
+	fill="none"
+	xmlns="http://www.w3.org/2000/svg"
+>
+	<path
+		d="M1.333 14.667h13.334"
+		stroke="#2E2320"
+		stroke-miterlimit="10"
+		stroke-linecap="round"
+		stroke-linejoin="round"
+	/>
+	<path
+		d="M6.5 2.667v12h3v-12c0-.734-.3-1.334-1.2-1.334h-.6c-.9 0-1.2.6-1.2 1.334Zm-4.5 4v8h2.667v-8c0-.734-.267-1.334-1.067-1.334h-.533c-.8 0-1.067.6-1.067 1.334ZM11.333 10v4.667H14V10c0-.733-.267-1.333-1.067-1.333H12.4c-.8 0-1.067.6-1.067 1.333Z"
+		stroke="#2E2320"
+		stroke-linecap="round"
+		stroke-linejoin="round"
+	/>
+</svg>
---
+++ src/icons/Smileys.svelte
@@ -0,0 +1,72 @@
+<script lang="ts">
+	export let size: number | null = null
+	export let w = 20
+	export let h = 20
+	export let weight: 'light' | 'bold' = 'light'
+
+	const strokeWidth = weight === 'light' ? 1.5 : 4
+</script>
+
+<svg
+	width={size || w}
+	height={size || h}
+	stroke-width={strokeWidth}
+	viewBox="0 0 16 16"
+	fill="none"
+	xmlns="http://www.w3.org/2000/svg"
+>
+	<path
+		d="M9.33331 4.1333V6.66666C7.47331 6.7 6.69998 7.47333 6.66665 9.33333H4.13331C2.13331 9.33333 1.33331 8.53336 1.33331 6.53336V4.1333C1.33331 2.1333 2.13331 1.33333 4.13331 1.33333H6.53331C8.53331 1.33333 9.33331 2.1333 9.33331 4.1333Z"
+		stroke="#2E2320"
+		stroke-linecap="round"
+		stroke-linejoin="round"
+	/>
+	<path
+		d="M4.64001 3.9133C4.28667 3.6733 3.82001 3.67331 3.46667 3.92665"
+		stroke="#2E2320"
+		stroke-miterlimit="10"
+		stroke-linecap="round"
+		stroke-linejoin="round"
+	/>
+	<path
+		d="M7.30669 3.9133C6.95336 3.6733 6.4867 3.67331 6.13336 3.92665"
+		stroke="#2E2320"
+		stroke-miterlimit="10"
+		stroke-linecap="round"
+		stroke-linejoin="round"
+	/>
+	<path
+		d="M5.43999 7.61336H3.89333C3.69333 7.61336 3.53333 7.45334 3.53333 7.25334C3.53333 6.26 4.33999 5.45337 5.33333 5.45337C5.75999 5.45337 6.15332 5.60001 6.45999 5.84668"
+		stroke="#2E2320"
+		stroke-miterlimit="10"
+		stroke-linecap="round"
+		stroke-linejoin="round"
+	/>
+	<path
+		d="M14.6667 9.46664V11.8667C14.6667 13.8667 13.8667 14.6667 11.8667 14.6667H9.46669C7.46669 14.6667 6.66669 13.8667 6.66669 11.8667V9.33334C6.70002 7.47334 7.47335 6.70001 9.33335 6.66667H11.8667C13.8667 6.66667 14.6667 7.46664 14.6667 9.46664Z"
+		stroke="#2E2320"
+		stroke-linecap="round"
+		stroke-linejoin="round"
+	/>
+	<path
+		d="M9.97332 9.08C9.61999 9.32 9.15332 9.31998 8.79999 9.06665"
+		stroke="#2E2320"
+		stroke-miterlimit="10"
+		stroke-linecap="round"
+		stroke-linejoin="round"
+	/>
+	<path
+		d="M12.64 9.08C12.2867 9.32 11.82 9.31998 11.4667 9.06665"
+		stroke="#2E2320"
+		stroke-miterlimit="10"
+		stroke-linecap="round"
+		stroke-linejoin="round"
+	/>
+	<path
+		d="M9.22664 10.7867H12.1066C12.3066 10.7867 12.4666 10.9466 12.4666 11.1466C12.4666 12.14 11.66 12.9467 10.6666 12.9467C9.67331 12.9467 8.86664 12.14 8.86664 11.1466C8.86664 10.9466 9.02664 10.7867 9.22664 10.7867Z"
+		stroke="#2E2320"
+		stroke-miterlimit="10"
+		stroke-linecap="round"
+		stroke-linejoin="round"
+	/>
+</svg>
